fe47662e9b27c1749d4a78e8ad60b58e0002d570
jeongerased <jeongjiun20@gmail.com>
2023-10-12 19:23:23 +0900

    프로젝트 시작점

---
+++ test.h
@@ -0,0 +1,5 @@
+#pragma once
+#include<iostream>
+void echo() {
+	std::cout << "자 프로젝트 시작해볼까?????" << std::endl;
+}
---
+++ tetrisMain.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+using namespace std;
+
+int main() {
+	echo();
+}
